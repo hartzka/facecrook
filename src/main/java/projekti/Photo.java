e427aec15f681ea61ed80c31ea00c2c0034ed6f4
Kai Hartzell <kai.hartzell@gmail.com>
2019-05-01 14:43:28 +0300

    yritys 5

--- src/main/java/projekti/Photo.java
+++ src/main/java/projekti/Photo.java
@@ -4,15 +4,13 @@ package projekti;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.Basic;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 import org.springframework.data.jpa.domain.AbstractPersistable;
  
 @Entity
@@ -30,6 +28,7 @@ public class Photo extends AbstractPersistable<Long>{
     @ManyToOne
     private Account user;
     
+    @Type(type="org.hibernate.type.BinaryType")
     private byte[] content;
     
     @OneToMany(mappedBy = "photo")
